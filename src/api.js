34f47c6909ff3447e71b16d1486c44c65989c6fe
Samuel Valmir <myprogrammingmaterial@gmail.com>
2022-07-06 12:59:24 -0300

    Update: Server is ready to deploy
    Its not functional yet but only with the enogth configurations

---
+++ src/api.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const router = express.Router();
+
+const app = express();
+const port = process.env.PORT || 9999;
+
+router.use(cors({origin: `http://192.168.100.20:${port}`}));
+
+app.use("/.netlify/functions/api", router);
+router.get("/", express.static(path.join("public")));
+
+module.exports = app;
+module.exports.handler = serverless(app);
