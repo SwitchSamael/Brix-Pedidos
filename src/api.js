f8a270869cf2e26210512e843498953a47d53cd0
Samuel Valmir <myprogrammingmaterial@gmail.com>
2022-07-08 10:11:44 -0300

    Update: Parse xlsx to Json e make a file with
    This occurs on server side

--- src/api.js
+++ src/api.js
@@ -5,13 +5,49 @@ const path = require("path");
 const fs = require("fs");
 const router = express.Router();
 
+const xlsx = require("xlsx");
+
+// const filesPath = path.join("dist", "files"); 
+const filesPath = path.join(__dirname, "..", "dist", "files"); 
+
+let workbook = xlsx.readFile(path.join(filesPath, "Table.xlsx"), { cellDates: true });
+let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+let data = xlsx.utils.sheet_to_json(worksheet);
+
+let formattedTableJson = {};
+
+let initialRowIndex = 4;
+
+data.map((rowObject, rowIndex) => {
+    if (rowIndex >= initialRowIndex && rowIndex <= 6) {
+        formattedTableJson[`row${rowIndex - initialRowIndex}`] = [];
+        Object.entries(rowObject).map((cellArray, cellIndex) => {
+            const cell = cellArray[1];
+            if (cellIndex <= 5 || cellIndex === 11) {
+                formattedTableJson[`row${rowIndex - initialRowIndex}`].push(cell);
+            }
+        })
+
+    }
+})
+
+console.log(formattedTableJson);
 const app = express();
 const port = process.env.PORT || 9999;
 
-router.use(cors({origin: `http://192.168.100.20:${port}`}));
+router.use(cors({ origin: `http://192.168.100.20:${port}` }));
 
-app.use("/.netlify/functions/api", router);
-router.get("/", express.static(path.join("dist")));
+// app.use("/.netlify/functions/api", router);
+// router.get("/", express.static(path.join("dist")));
 
-module.exports = app;
-module.exports.handler = serverless(app);
+app.use("/", express.static(path.join("dist")));
+
+
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
