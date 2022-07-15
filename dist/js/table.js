d5ff9cec99cf7462000352c93c07e1415b13b09c
Samuel Valmir <myprogrammingmaterial@gmail.com>
2022-07-15 18:30:20 -0300

    Update: Seleted items list
    -When user changes the amount of the item in the table, a new element is created on the selected items container with the item's description.
    
    -Two tables design (The code is useful but not clean due to the urgency necessity of the project. But it will sure be cleaned).

--- dist/js/table.js
+++ dist/js/table.js
@@ -9,6 +9,7 @@ const checkboxList = { "row": [], "column": [] };
 table.appendChild(tableHead);
 table.appendChild(tableBody);
 
+const selectedItemsObjectList = [];
 
 //Remove focus after 2 seconds
 // let allButtons = document.getElementsByClassName("btn");
@@ -18,27 +19,53 @@ table.appendChild(tableBody);
 //     })
 // })
 
-function writeTable(rows, includeCheckbox = true, defaultTable = true) {
+function writeTable(rows,
+
+    defaultTable = true
+    // bothCheckbox,
+    // leftCheckbox,
+    // topCheckbox,
+    // bothIndex,
+    // leftIndex,
+    // topIndex,
+    // inputColumn
+
+) {
+
+    // if (defaultTable) {
+    //     console.log(123)
+    //     leftCheckbox, topCheckbox, inputColumn = false;
+    // };
+
+    // if (bothCheckbox) {
+    //     leftCheckbox, topCheckbox = true;
+    // };
+
+    // if (bothIndex) {
+    //     leftIndex, topIndex = true;
+    // };
+
     return new Promise(async resolve => {
         await rows.forEach((row, rowIndex) => {
             const rowElementBody = document.createElement("tr");
             let charCode = 65;
             let firstRepetitionOfColumnName = true;
             let columnNameArray = [String.fromCharCode(charCode)];
+            let oldPrice = 0;
 
             // Insert the table head
-            if (includeCheckbox && rowIndex === 0) {
-
+            if (defaultTable && rowIndex === 0) {
                 let rowElementHead = document.createElement("tr");
                 insertCell(rowElementHead, "", "th", null, null);
                 insertCell(rowElementHead, "", "th", null, null);
 
                 row.forEach(() => {
-                    insertCell(rowElementHead, "", "th", rowIndex, null, true);
+                    insertCell(rowElementHead, "", "th", rowIndex, null, "checkbox");
                 });
 
                 rowElementHead = document.createElement("tr");
                 insertCell(rowElementHead, "", "th", null, null);
+
                 insertCell(rowElementHead, "#", "th", null, null);
 
                 row.forEach(() => {
@@ -74,27 +101,47 @@ function writeTable(rows, includeCheckbox = true, defaultTable = true) {
     });
 
 
-    function insertCell(rowElement, cellContent, type, rowIndex = null, cellIndex = null, isCheckbox = false) {
+    function insertCell(rowElement, cellContent, cellType, rowIndex = null, cellIndex = null, elementType = "text") {
         rowElement.classList.add("text-center");
         // rowElement.setAttribute("data-align", "center");
 
         // Insert the checkbox and index of the row (two new cells)
-        if (includeCheckbox && cellIndex === 0) {
-            insertCell(rowElement, "", "th", null, null, true);
-            insertCell(rowElement, rowIndex + 1, "th");
-        } else if(rowIndex === 0){
-            type = "th";
-        }/
-
-        if(defaultTable && cellIndex === 0){
-            insertCell(rowElement, "", "td")
+        if (cellIndex === 0) {
+            if (defaultTable) {
+                insertCell(rowElement, "", "th", null, null, "checkbox");
+                insertCell(rowElement, rowIndex + 1, "th");
+            } else if (rowIndex === 0) {
+                // Insert empty cell in first column
+                insertCell(rowElement, "", "th", null, null);
+            } else {
+                // Insert checkbox in first column
+                insertCell(rowElement, "", "th", null, null, "checkbox");
+            };
         };
 
-        const cellElement = document.createElement(type);
+        if (rowIndex === 0) {
+            cellType = "th";
+        };
 
-        if (isCheckbox === true) {
-            const checkbox = createCheckbox(rowIndex);
+        // Insert new columns (before index)
+        if (!defaultTable) {
+            // Insert input column
+            if (rowIndex === 0 && cellIndex === 0) {
+                insertCell(rowElement, "Quantidade", "th", null, null);
+            } else if (cellIndex === 0) {
+                insertCell(rowElement, "", "td", null, null, "input");
+            };
+        };
+
+        const cellElement = document.createElement(cellType);
+
+        if (elementType === "checkbox") {
+            const checkbox = defaultTable ? createCheckbox(rowIndex) : createCheckbox(rowIndex, true);
+
             cellElement.appendChild(checkbox);
+        } else if (elementType === "input") {
+            const input = createInputText(rowIndex);
+            cellElement.appendChild(input);
         } else {
             cellElement.innerHTML = cellContent;
         };
@@ -102,18 +149,118 @@ function writeTable(rows, includeCheckbox = true, defaultTable = true) {
         rowElement.appendChild(cellElement);
         tableBody.appendChild(rowElement);
 
+
+        // Insert new columns (after index)
+        if (!defaultTable) {
+
+            // Add the money sign in "CE column's cells"
+            if (rowIndex !== 0 && cellIndex === 6) {
+                oldPrice = Number(cellContent);
+                cellElement.innerHTML = oldPrice + " R$";
+            };
+
+            // Insert price column
+            if (rowIndex === 0 && cellIndex === 6) {
+
+                insertCell(rowElement, "Preço", "th", null, null);
+            } else if (cellIndex === 6) {
+                const newPrice = (oldPrice + (oldPrice * 0.4) + (oldPrice * 0.6)).toFixed(2) + " R$";
+                insertCell(rowElement, newPrice, "td", null, null);
+            };
+        };
+
     };
 
-    function createCheckbox(rowIndex) {
+    function createCheckbox(rowIndex, disabled = false) {
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.classList.add("form-check-input", "custom-check-danger");
+
+        if (disabled) {
+            checkbox.setAttribute("disabled", true);
+        };
 
         if (rowIndex === 0) checkboxList.column.push(checkbox);
         else checkboxList.row.push(checkbox);
 
         return checkbox;
     };
+
+    function createInputText() {
+        const input = document.createElement("input");
+        input.type = "number";
+        input.min = 0;
+        input.classList.add("form-control", "custom-check-danger");
+        input.style.width = "4rem";
+        input.style.padding = "5px";
+
+        input.addEventListener("input", (e) => {
+            const input = e.target;
+            const row = e.target.parentElement.parentElement;
+            const checkbox = row.children[0].children[0];
+            const amount = input.value;
+            const id = row.children[2].innerText;
+
+            if (amount === "0" || amount === "") {
+                const selectedItemObject = checkIfSelectedItemAlreadyExists(id)
+                const selectedItemObjectListIndex = selectedItemsObjectList.indexOf(selectedItemObject);
+                selectedItemsObjectList.splice(selectedItemObjectListIndex, 1);
+
+                input.value = "";
+
+                checkbox.checked = false;
+            } else {
+                checkbox.checked = true;
+
+                const itemDescription = row.children[5].innerText;
+                const price = row.children[9].innerText.split(" ")[0];
+                const totalPrice = (amount * price).toFixed(2);
+
+                if (selectedItemObject = checkIfSelectedItemAlreadyExists(id)) {
+                    selectedItemObject.amount = amount;
+                    selectedItemObject.itemDescription = itemDescription;
+                    selectedItemObject.totalPrice = totalPrice;
+                } else {
+                    selectedItemsObjectList.push({ id, amount, itemDescription, totalPrice });
+                };
+
+
+            };
+
+            updateSelectedItemsContainer();
+        });
+
+        return input;
+    };
+};
+
+function checkIfSelectedItemAlreadyExists(id) {
+    const filtered = selectedItemsObjectList.filter(selectedItemObject => selectedItemObject.id === id);
+
+    if (filtered.length === 1) return filtered[0];
+    return false;
+};
+
+function updateSelectedItemsContainer() {
+    const selectedItemsContainer = document.querySelector("#selectedItems");
+    selectedItemsContainer.innerHTML = "";
+
+    selectedItemsObjectList.forEach(selectedItemObject => {
+        selectedItemsContainer.innerHTML += createSelectedItem(selectedItemObject.id,
+            selectedItemObject.amount,
+            selectedItemObject.itemDescription,
+            selectedItemObject.totalPrice);
+    });
+};
+
+function createSelectedItem(id, amount, description, totalPrice) {
+    return `
+        <div class="d-flex justify-content-evenly" id="${id}">
+            <div class="selected-item-amount">${amount} x</div>
+            <div class="selected-item-description">${description} </div>
+            <div class="selected-item-total-price">${totalPrice} R$</div>
+            <div class="btn btn-close text-bg-danger"></div>
+        </div>`;
 };
 
 function disableScroll() {
